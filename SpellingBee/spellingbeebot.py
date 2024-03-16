42e651e7a62ca86dd5a741d6fe37dbb78f517889
Aaryan Dave <aaryan.r.dave@gmail.com>
2024-03-16 12:03:25 -0400

    Clean up SpellingBeeBot.py

--- SpellingBee/spellingbeebot.py
+++ SpellingBee/spellingbeebot.py
@@ -1,15 +1,24 @@
-center_letter = "t"
-table_letters = set("gniahb")
-table_letters.add(center_letter)
+center_letter = input("Center letter: ")
+remaining_letters = set(input("Remaining letters: "))
+remaining_letters.add(center_letter)
 
 valid_words = set()
 
 # Preprocess the dictionary to remove unnecessary characters
 with open('../../wordlist.txt', 'r') as input_dict_file:
     for line in input_dict_file:
-        word = line.strip().lower().replace("'", '')
-        if len(word) > 3 and center_letter in word and all(letter in table_letters for letter in word):
+        word = line.strip().lower()
+        if len(word) > 3 and center_letter in word and all(letter in remaining_letters for letter in word):
             valid_words.add(word)
 
 print(f'Num Words: {len(valid_words)}')
-print(sorted(valid_words, key=lambda x: len(x), reverse=True)) 
+valid_words = sorted(valid_words, key=lambda x: len(x), reverse=True)
+print("Longest words: ", valid_words[:5])
+
+# write the words to spellingbee_solutions.txt
+with open('spellingbee_solutions.txt', 'w') as output_file:
+    output_file.write(f"Num Words: {len(valid_words)}\n")
+    for word in valid_words:
+        output_file.write(word + "\n")
+
+print("Remaining solutions in spellingbee_solutions.txt.")
