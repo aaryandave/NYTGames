b584960991941ecf2e243eeb6b591247d7fc8ba0
Aaryan Dave <aaryan.r.dave@gmail.com>
2024-07-04 01:00:16 -0400

    Fix pylint issues in spellingbeebot.py

--- SpellingBee/spellingbeebot.py
+++ SpellingBee/spellingbeebot.py
@@ -1,3 +1,5 @@
+"""NYTGames Spelling Bee Solver
+"""
 center_letter = input("Center letter: ")
 remaining_letters = set(input("Remaining letters: "))
 remaining_letters.add(center_letter)
@@ -5,18 +7,19 @@ remaining_letters.add(center_letter)
 valid_words = set()
 
 # preprocess the dictionary to remove unnecessary characters
-with open('../../wordlist.txt', 'r') as input_dict_file:
+with open('../../wordlist.txt', 'r', encoding="utf-8") as input_dict_file:
     for line in input_dict_file:
         word = line.strip().lower()
-        if len(word) > 3 and center_letter in word and all(letter in remaining_letters for letter in word):
+        if len(word) > 3 and center_letter in word and all(letter in
+                                                           remaining_letters for letter in word):
             valid_words.add(word)
 
 print(f'Num Words: {len(valid_words)}')
-valid_words = sorted(valid_words, key=lambda x: len(x), reverse=True)
-print("Longest words: ", valid_words[:5])
+valid_words_list: list = sorted(valid_words, key=len, reverse=True)
+print("Longest words: ", valid_words_list[:5])
 
 # write the words to spellingbee_solutions.txt
-with open('spellingbee_solutions.txt', 'w') as output_file:
+with open('spellingbee_solutions.txt', 'w', encoding="utf-8") as output_file:
     output_file.write(f"Num Words: {len(valid_words)}\n")
     for word in valid_words:
         output_file.write(word + "\n")
